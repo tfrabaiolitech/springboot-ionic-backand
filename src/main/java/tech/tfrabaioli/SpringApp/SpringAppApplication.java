b388885aa8985fa059aa8befd961a206f067cbf9
tfrabaiolitech <tfrabaioli.tech@gmail.com>
2019-05-08 21:58:03 +0100

    Criando instanciação com o CommandLineRunner

--- src/main/java/tech/tfrabaioli/SpringApp/SpringAppApplication.java
+++ src/main/java/tech/tfrabaioli/SpringApp/SpringAppApplication.java
@@ -1,13 +1,32 @@
 package tech.tfrabaioli.SpringApp;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import tech.tfrabaioli.SpringApp.domain.Categoria;
+import tech.tfrabaioli.SpringApp.repositories.CategoriaRepository;
+
 @SpringBootApplication
-public class SpringAppApplication {
+public class SpringAppApplication implements CommandLineRunner {
+	
+	@Autowired
+	CategoriaRepository categoriaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringAppApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritorio");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1,cat2));
 	}
 
 }
