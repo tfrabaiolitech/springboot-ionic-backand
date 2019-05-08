98b546a632df204ce3201afaf0ce29fb29cab17a
tfrabaiolitech <tfrabaioli.tech@gmail.com>
2019-05-08 21:54:12 +0100

    Criando Repository e service para Categoria

--- src/main/java/tech/tfrabaioli/SpringApp/resources/CategoriaResource.java
+++ src/main/java/tech/tfrabaioli/SpringApp/resources/CategoriaResource.java
@@ -1,28 +1,30 @@
 package tech.tfrabaioli.SpringApp.resources;
 
-import java.util.ArrayList;
-import java.util.List;
 
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import tech.tfrabaioli.SpringApp.domain.Categoria;
+import tech.tfrabaioli.SpringApp.services.CategoriaService;
 
 @RestController //Controlador Rest
 @RequestMapping(value="categorias") // Nome do endpoint Rest
 public class CategoriaResource {
 	
-	@RequestMapping(method = RequestMethod.GET) //verbo HTTP de GET
-	public List<Categoria> listar() {
+	@Autowired
+	private CategoriaService service;
+	
+	@RequestMapping(value= "/{id}", method = RequestMethod.GET) //verbo HTTP de GET
+	
+	public ResponseEntity<?> find(@PathVariable Integer id) {
 		
-		Categoria cat1 = new Categoria(1, "Informática");
-		Categoria cat2 = new Categoria(2, "Escritorio");
-		
-		List<Categoria> lista = new ArrayList<>(); 
-		lista.add(cat1);
-		lista.add(cat2);
-		return lista;
+		Categoria obj = service.find(id);
+		return ResponseEntity.ok().body(obj);
 	}
 
 }
