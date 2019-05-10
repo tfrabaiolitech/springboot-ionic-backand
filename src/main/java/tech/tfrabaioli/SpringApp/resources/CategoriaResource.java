ca1a9df7739f99c1f0521b2b178c73dd05b9806f
tfrabaiolitech <tfrabaioli.tech@gmail.com>
2019-05-10 20:00:30 +0100

    Inserindo Categoria como POST

--- src/main/java/tech/tfrabaioli/SpringApp/resources/CategoriaResource.java
+++ src/main/java/tech/tfrabaioli/SpringApp/resources/CategoriaResource.java
@@ -2,12 +2,18 @@ package tech.tfrabaioli.SpringApp.resources;
 
 
 
+import java.net.URI;
+
+import javax.servlet.Servlet;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import tech.tfrabaioli.SpringApp.domain.Categoria;
 import tech.tfrabaioli.SpringApp.services.CategoriaService;
@@ -26,5 +32,16 @@ public class CategoriaResource {
 		Categoria obj = service.find(id);
 		return ResponseEntity.ok().body(obj);
 	}
+	//Padrão do framework 
+@RequestMapping(method = RequestMethod.POST)	
+  public ResponseEntity<Void> insert (@RequestBody Categoria obj){
+	  obj = service.insert(obj);
+	  URI uri = ServletUriComponentsBuilder 
+			  .fromCurrentRequest() // Pega a url por exemplo localhost:8080/categorias
+			  .path("/{id}") //Caminho do objecto criado
+			  .buildAndExpand(obj.getId()) //Atribui o valor ao id
+			  .toUri();
+	  return ResponseEntity.created(uri).build();
+  }
 
 }
