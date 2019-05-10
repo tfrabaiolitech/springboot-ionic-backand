cb36d863e7e7849c0ecfa89d662a34ff722c3e66
tfrabaiolitech <tfrabaioli.tech@gmail.com>
2019-05-10 20:49:12 +0100

    Paginação com parâmetros opcionais na requisição

--- src/main/java/tech/tfrabaioli/SpringApp/services/CategoriaService.java
+++ src/main/java/tech/tfrabaioli/SpringApp/services/CategoriaService.java
@@ -6,6 +6,9 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import tech.tfrabaioli.SpringApp.domain.Categoria;
@@ -50,9 +53,13 @@ public class CategoriaService {
 		}
 	}
 
-	public List<Categoria> findAll() {
-		
+	public List<Categoria> findAll() {	
 		return repo.findAll();
+	}
+	
+	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		return repo.findAll(pageRequest);
 	}
 
 	
