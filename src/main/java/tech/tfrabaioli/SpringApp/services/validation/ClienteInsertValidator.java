7ebf6395f698ef3d3e7ee02fe75df902813936f9
tfrabaiolitech <tfrabaioli.tech@gmail.com>
2019-05-12 19:00:22 +0100

    Validação customizada CPF ou CPNJ na inserção de um Cliente

---
+++ src/main/java/tech/tfrabaioli/SpringApp/services/validation/ClienteInsertValidator.java
@@ -0,0 +1,42 @@
+package tech.tfrabaioli.SpringApp.services.validation;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+import tech.tfrabaioli.SpringApp.domain.enums.TipoCliente;
+import tech.tfrabaioli.SpringApp.dto.ClienteNewDTO;
+import tech.tfrabaioli.SpringApp.resources.exceptions.FieldMessage;
+import tech.tfrabaioli.SpringApp.services.validation.utils.BR;
+
+
+public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+
+	@Override
+	public void initialize(ClienteInsert ann) {
+	}
+
+	@Override
+	public boolean isValid(ClienteNewDTO objDto, ConstraintValidatorContext context) {
+
+		List<FieldMessage> list = new ArrayList<>();
+
+		if (objDto.getTipo().equals(TipoCliente.PESSOAFISICA.getCod()) && !BR.isValidCPF(objDto.getCpfOuCnpj())) {
+			list.add(new FieldMessage("cpfOuCnpj", "CPF inválido"));
+		}
+
+		if (objDto.getTipo().equals(TipoCliente.PESSOAJURIDICA.getCod()) && !BR.isValidCNPJ(objDto.getCpfOuCnpj())) {
+			list.add(new FieldMessage("cpfOuCnpj", "CNPJ inválido"));
+		}
+
+		for (FieldMessage e : list) {
+			context.disableDefaultConstraintViolation();
+			context.buildConstraintViolationWithTemplate(e.getMessage()).addPropertyNode(e.getFieldName())
+					.addConstraintViolation();
+		}
+		return list.isEmpty();
+	}
+}
