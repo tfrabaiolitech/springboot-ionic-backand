f52b585c4af8837e8f535a9f5093d662e0cfdf53
tfrabaiolitech <tfrabaioli.tech@gmail.com>
2019-05-12 19:05:55 +0100

    Validação customizada email em Cliente

--- src/main/java/tech/tfrabaioli/SpringApp/repositories/ClienteRepository.java
+++ src/main/java/tech/tfrabaioli/SpringApp/repositories/ClienteRepository.java
@@ -2,10 +2,14 @@ package tech.tfrabaioli.SpringApp.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import tech.tfrabaioli.SpringApp.domain.Cliente;
 
 @Repository
 public interface ClienteRepository extends JpaRepository<Cliente, Integer> {
+	
+	@Transactional(readOnly=true)
+	Cliente findByEmail(String email);
 
 }
--- src/main/java/tech/tfrabaioli/SpringApp/services/validation/ClienteInsertValidator.java
+++ src/main/java/tech/tfrabaioli/SpringApp/services/validation/ClienteInsertValidator.java
@@ -7,13 +7,21 @@ import java.util.List;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import tech.tfrabaioli.SpringApp.domain.Cliente;
 import tech.tfrabaioli.SpringApp.domain.enums.TipoCliente;
 import tech.tfrabaioli.SpringApp.dto.ClienteNewDTO;
+import tech.tfrabaioli.SpringApp.repositories.ClienteRepository;
 import tech.tfrabaioli.SpringApp.resources.exceptions.FieldMessage;
 import tech.tfrabaioli.SpringApp.services.validation.utils.BR;
 
 
 public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+	
+	@Autowired
+	private ClienteRepository repo;
+
 
 	@Override
 	public void initialize(ClienteInsert ann) {
@@ -30,6 +38,11 @@ public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert
 
 		if (objDto.getTipo().equals(TipoCliente.PESSOAJURIDICA.getCod()) && !BR.isValidCNPJ(objDto.getCpfOuCnpj())) {
 			list.add(new FieldMessage("cpfOuCnpj", "CNPJ inválido"));
+		}
+		
+		Cliente aux = repo.findByEmail(objDto.getEmail());
+		if (aux != null) {
+			list.add(new FieldMessage("email", "Email já existente"));
 		}
 
 		for (FieldMessage e : list) {
