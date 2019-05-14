29e24607e6022a5d4c0adf246b2bb8d5d4ae20a0
tfrabaiolitech <tfrabaioli.tech@gmail.com>
2019-05-14 13:21:45 +0100

    Enviando email via smtp gmail

--- src/main/java/tech/tfrabaioli/SpringApp/config/DevConfig.java
+++ src/main/java/tech/tfrabaioli/SpringApp/config/DevConfig.java
@@ -11,6 +11,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 import tech.tfrabaioli.SpringApp.services.DBService;
+import tech.tfrabaioli.SpringApp.services.EmailService;
+import tech.tfrabaioli.SpringApp.services.SmtpEmailService;
 
 @Configuration
 @Profile("dev")
@@ -31,5 +33,10 @@ public class DevConfig {
 
 		dbService.instantiateTestDatabase();
 		return true;
+	}
+	
+	@Bean
+	public EmailService emailService() {
+		return new SmtpEmailService();
 	}
 }
