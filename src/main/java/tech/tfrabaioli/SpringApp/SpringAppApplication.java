6ddb345ac54e823012903db8081c0b18a71f0409
tfrabaiolitech <tfrabaioli.tech@gmail.com>
2019-05-18 20:50:16 +0100

    Salvando arquivo no S3

--- src/main/java/tech/tfrabaioli/SpringApp/SpringAppApplication.java
+++ src/main/java/tech/tfrabaioli/SpringApp/SpringAppApplication.java
@@ -1,14 +1,19 @@
 package tech.tfrabaioli.SpringApp;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import tech.tfrabaioli.SpringApp.services.S3Service;
 
 
 @SpringBootApplication
 public class SpringAppApplication implements CommandLineRunner {
 	
+	@Autowired
+	private S3Service s3Service;
 	
 
 	public static void main(String[] args) {
@@ -16,8 +21,7 @@ public class SpringAppApplication implements CommandLineRunner {
 	}
 
 	@Override
-	public void run(String... args) throws Exception {
-		
-		
-	}
+	public void run(String... args) throws Exception {	
+		s3Service.uploadFile("/Users/tfrabaiolitech/Pictures/aws.jpg");
+	}	
 }
